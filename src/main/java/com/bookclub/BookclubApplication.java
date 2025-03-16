22648ae1609f03ae7cd0063ce62040942635a424
Matthew Kalahar <mjkalahar@gmail.com>
2025-03-16 17:07:20 -0500

    Add comments

--- src/main/java/com/bookclub/BookclubApplication.java
+++ src/main/java/com/bookclub/BookclubApplication.java
@@ -1,11 +1,22 @@
+/*
+ * Kalahar, M. (2025). CIS 530 Intermediate Java Programming. Bellevue University.
+ */
 package com.bookclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * This class represents a simple Bookclub web application
+ */
 @SpringBootApplication
 public class BookclubApplication {
 
+	/**
+     * Main method to launch the application.
+     *
+     * @param args Command line arguments.
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(BookclubApplication.class, args);
 	}
